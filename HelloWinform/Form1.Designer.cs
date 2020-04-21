f3e763f2ab3cb57af5a82174e6235ee06ea0fa1b
dayeon1021 <s2018s20@e-mirim.hs.kr>
2020-04-21 14:27:22 +0900

    버튼이벤트

--- HelloWinform/Form1.Designer.cs
+++ HelloWinform/Form1.Designer.cs
@@ -46,7 +46,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(410, 300);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
